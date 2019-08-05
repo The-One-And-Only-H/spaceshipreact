caca6ebd24443e88a73bb3e4b662f7dde3845a9b
H <juana.galan@outlook.com>
2019-08-05 16:50:38 +0100

    Adds logo

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import ReactSVG from 'react-svg';
-import NBLogo from './NBLogo.svg';
+import { ReactComponent as NBLogo } from './NBLogo.svg';
 
 export default class LandingPage extends Component {
   constructor(props) {
@@ -12,31 +11,7 @@ export default class LandingPage extends Component {
   render() {
     return (
       <>
-        {/* <ReactSVG className="Logo" src={NBLogo.svg} alt="Neverbland" /> */}
-        <ReactSVG
-          src="NBLogo.svg"
-          className="Logo"
-          afterInjection={(error, svg) => {
-            if (error) {
-              console.error(error);
-              return;
-            }
-            console.log(svg);
-          }}
-          beforeInjection={(svg) => {
-            svg.classList.add('svg-class-name');
-            svg.setAttribute('style', 'width: 200px');
-          }}
-          evalScripts="always"
-          fallback={() => <span>Error!</span>}
-          loading={() => <span>Loading</span>}
-          renumerateIRIElements={false}
-          wrapper="span"
-          className="wrapper-class-name"
-          onClick={() => {
-            console.log('wrapper onClick');
-          }}
-        />
+        <NBLogo className="Logo" ariaLabel="Logo" />
       </>
     );
   }
