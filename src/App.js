0b0a6f3afd031449bcd1b90528ef53a9506cb9b7
H <juana.galan@outlook.com>
2020-04-25 14:58:12 +0100

    Detects multiple collisions

--- src/App.js
+++ src/App.js
@@ -7,23 +7,36 @@ import Score from './Score';
 import Astronaut from './Astronaut';
 
 // To do
-// - increment the timer only when the player has interacted with specific items in the game
-// - decrement the timer when the players has interacted with other items in the game
+// - increment the score only when the player has interacted with specific items in the game
+// - decrement the score when the players has interacted with other items in the game
 // - create an intro and outright sequence
 // - detect the right key for when the player fires and subsequently the result of what happens when that interacts with an item
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    const astronauts = [];
+    for (let z = 0; z < 5; z++) {
+      astronauts.push(<Astronaut key={z} />);
+    }
+
+    this.state = {
+      astronauts,
+    };
+  }
+
   detectCollision() {
     const ship = document.getElementsByClassName('spaceship');
     if (ship.length === 0) {
     }
     const shipRect = ship[0].getBoundingClientRect();
-    console.log('ship:', shipRect);
+    // console.log('ship:', shipRect);
 
     const astro = document.getElementsByClassName('astronaut');
-    for (let y = 0; y < 1; y++) {
+    for (let y = 0; y < 5; y++) {
       const astroRect = astro[y].getBoundingClientRect();
-      console.log('astro:', astroRect);
+      // console.log('astro:', astroRect);
       if (
         astroRect.x > shipRect.left
         && astroRect.x < shipRect.right
@@ -52,17 +65,10 @@ class App extends Component {
       stars.push(<Star key={i} />);
     }
 
-    const astronauts = [];
-    for (let z = 0; z < 1; z++) {
-      astronauts.push(<Astronaut key={z} />);
-    }
-
     return (
       <div>
         <LandingPage />
-        {/* {elemRect.top - bodyRect.top !== 0 ? <div className="astro">{astronauts}</div> : null} */}
-        {/* <div className="astronaut">{astronauts}</div> */}
-        {astronauts}
+        {this.state.astronauts}
         {stars}
         <Spaceship className="spaceship" />
         <Score />
